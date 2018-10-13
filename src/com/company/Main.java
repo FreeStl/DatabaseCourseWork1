296563c1ec4ca3b4d995fc135f01b1a55b2fcc17
Max <munazaruk@gmail.com>
2018-10-13 16:05:08 +0300

    Second Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args){
+        //1
+        //PrepStat.userOutDate1();
+
+        //2
+        //PrepStat.userWait2();
+        //PrepStat.userWait2("Мазь");
+
+        //3
+        //PrepStat.popularMedsTop3();
+        //PrepStat.popularMedsTop3("Таблетки");
+
+        //4
+        /*Calendar from = Calendar.getInstance();
+        Calendar till = Calendar.getInstance();
+        from.set(2018,9,1);
+        till.set(2018,10,5);
+        PrepStat.medsUsedForPeriod4("Пента",from.getTimeInMillis(),till.getTimeInMillis());
+        */
+
+        //5
+        /*Calendar from = Calendar.getInstance();
+        Calendar till = Calendar.getInstance();
+        from.set(2018,9,1);
+        till.set(2018,10,5);
+        PrepStat.userOrderList5("Пентанол",from.getTimeInMillis(),till.getTimeInMillis());
+        */
+        /*Calendar from = Calendar.getInstance();
+        Calendar till = Calendar.getInstance();
+        from.set(2018,9,1);
+        till.set(2018,10,5);
+        PrepStat.userOrderListCategory5("Таблетки",from.getTimeInMillis(),till.getTimeInMillis());
+        */
+
+        //6&7
+        //PrepStat.outOfMeds67(4);
+        //PrepStat.outOfMeds67("Мазь",4);
+
+        //8
+        //PrepStat.ordersInProd8();
+
+        //9
+        //PrepStat.medsComponents9();
+
+        //10
+        //PrepStat.medsTechnology10();
+        //PrepStat.medsTechnology10("Пентанол");
+        //PrepStat.medsTechnologyCategory10("Мазь");
+
+        //11
+        //PrepStat.medsPrice11("Пентанол");
+        //PrepStat.compPrice11("Пентанол");
+
+        //12
+        //PrepStat.medsInfo13("Пентанол");
+    }
+}
